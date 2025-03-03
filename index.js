214e1f650b38b63185372aee4165d0e2809695d3
shahrad0 <hunterlogan132@gmail.com>
2025-03-03 18:53:41 +0400

    made the code cleaner and some naming changes

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ let bombsPositions = []
 
 // adds tiles
 for(let i = 0; i < surface; i++){
-    board.appendChild(createCustomElement("div",{id:`${i}`,className:"row",}))
-    for(let j = 0; j < surface; j++){
+    board.appendChild(createCustomElement("div", { id: `${i}`, className: "row" }))
+    for (let j = 0; j < surface; j++) {
         document.getElementById(`${i}`).appendChild(createCustomElement("div", { id:`${i}-${j}`, className:"tile", onClick: mark }))
-    }   
+    }
 }
 
 // create random bombs
@@ -18,45 +18,71 @@ function randomBombTile(){
     const bomb = document.getElementById(`${bombPosition[0]}-${bombPosition[1]}`)
 
     if (bomb.classList.contains("red")) randomBombTile()
-    else bomb.classList.add("red")
+    else {
+        bomb.classList.add("red")
+        bombsPositions.push(bombPosition)
+    }
 }
+
 for (let i = 0; i < bombs; i++){
     randomBombTile()
 }
+
 function mark(event) {
     const clickedElement = event.target
     if (clickedElement.classList.contains("red")) window.alert("you lose")
     if (!clickedElement.classList.contains("clicked")) clickedElement.classList.add("clicked") 
-    
-    let surroundingBombs   = 0
-    let tempTileRow = ""
-    let tempTile = ""
+
+    let horizontalTile = ""
+    let verticalTile = ""
     let reached = false
-    for(let i = 0;i < clickedElement.id.length;i++ ){
+    for (let i = 0;i < clickedElement.id.length;i++ ) {
         if (clickedElement.id[i] === '-') {
             reached = true
             continue
         }
-        reached ? tempTileRow += clickedElement.id[i] : tempTile += clickedElement.id[i]
+        reached ? verticalTile += clickedElement.id[i] : horizontalTile += clickedElement.id[i]
     }
-    let tile =  []
-    tempTile = parseInt(tempTile)
-    tempTileRow = parseInt(tempTileRow)
-    if      (!tempTile === 0)    tile = [tempTile,tempTile+1]
-    else if (!tempTile === surface-1)    tile = [tempTile,tempTile-1]
-    else tile = [tempTile, tempTile-1, tempTile+1]
+
+    horizontalTile = parseInt(horizontalTile)
+    verticalTile = parseInt(verticalTile)
+
+    let clickedPosition = { 
+        horizontal: horizontalTile,
+        vertical: verticalTile
+    }
+
+    console.log(clickedPosition)
+    checkForBomb(clickedPosition, clickedElement)
+}
+
+function revealSurrounding(position) {
     
-    let tileRow =  []
-    if      (tempTileRow === 0)    tileRow = [tempTileRow,tempTileRow+1]
-    else if (tempTileRow === surface-1)    tileRow = [tempTileRow,tempTileRow-1]
-    else tileRow = [tempTileRow,tempTileRow-1,tempTileRow+1]
-    
-    tileRow.forEach(element => {
-        tile.forEach(delement => {
-            if (document.getElementById(`${delement}-${element}`).classList.contains("red")) surroundingBombs +=1
-        });
-    });
-    clickedElement.innerText = surroundingBombs ? surroundingBombs : null
+}
+
+function checkForBomb({ horizontal, vertical }, clickedElement) {
+    let surroundingTiles = { horizontal, vertical }
+    let surroundingBombs = 0
+ 
+    if (!horizontal === 0) surroundingTiles.horizontal = [horizontal, horizontal + 1]
+    else if (!horizontal === surface - 1) surroundingTiles.horizontal = [horizontal, horizontal - 1]
+    else surroundingTiles.horizontal = [horizontal, horizontal - 1, horizontal + 1]
+
+    if (vertical === 0) surroundingTiles.vertical = [vertical, vertical + 1]
+    else if (vertical === surface - 1) surroundingTiles.vertical = [vertical, vertical - 1]
+    else surroundingTiles.vertical = [vertical, vertical - 1, vertical + 1]
+
+    surroundingTiles.vertical.forEach(vertical => {
+        surroundingTiles.horizontal.forEach(horizontal => {
+            if (document.getElementById(`${horizontal}-${vertical}`).classList.contains("red")) surroundingBombs += 1
+        })
+    })
+
+    if (surroundingBombs === 0) {
+        clickedElement.innerText = 0
+        revealSurrounding({ horizontal, vertical })
+    }
+    else clickedElement.innerText = surroundingBombs
 }
 
 function createCustomElement(tag, { id = "", className = "", onClick = null }) {
